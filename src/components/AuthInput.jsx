2eb553b0c5801724b35091ad3055c3c4f4d8aa86
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-08 14:35:18 +0800

    feat: update signup/login page with adding AuthInput props

--- src/components/AuthInput.jsx
+++ src/components/AuthInput.jsx
@@ -14,7 +14,6 @@ const StyledLabel = styled.label`
   color: '#696974';
   text-align: start;
 `;
-
 const StyledInput = styled.input`
   outline: none;
   border: none;
@@ -22,11 +21,16 @@ const StyledInput = styled.input`
   border-radius: 0px;
 `;
 
-const AuthInput = () => {
+const AuthInput = ({ type, label, value, placeholder, onChange }) => {
   return (
     <StyledContainer>
-      <StyledLabel>label</StyledLabel>
-      <StyledInput type="text" placeholder="placeholder" />
+      <StyledLabel>{label}</StyledLabel>
+      <StyledInput
+        type={type || 'text'}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </StyledContainer>
   );
 };
