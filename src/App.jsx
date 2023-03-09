8195fed9b1d3d9d5b3f68620b7aac8f8629f1f4c
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-09 12:57:39 +0800

    feat: add CheckToken useEffect to context & export context hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { AuthContextProvider } from 'contexts/AuthContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
@@ -6,13 +7,15 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="todos" element={<TodoPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          {/* * 代表除了以上設定好的路由，其他字串不符合的會被導引到 homepage */}
-          <Route path="*" element={<HomePage />} />
-        </Routes>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="todos" element={<TodoPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            {/* * 代表除了以上設定好的路由，其他字串不符合的會被導引到 homepage */}
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,7 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState, useContext } from 'react';
 import * as jwt from 'jsonwebtoken';
-import { login, register } from 'api/auth';
+import { checkPermission, login, register } from '../api/auth';
+import { useLocation } from 'react-router-dom';
 
 const defaultAuthContext = {
   isAuthenticated: false, // 是否登入的判斷，取得憑證會更改為 true
@@ -13,9 +14,43 @@ const defaultAuthContext = {
 
 const AuthContext = createContext(defaultAuthContext);
 
+// 要寫成箭頭函式是因為 useContext 要在元件第一層被調用，不能在這裡就直接調用
+export const useAuthContext = () => useContext(AuthContext);
+
 export const AuthContextProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
+
+  // 取得當前頁面的 pathname 用來判斷當前頁面有沒有改變
+  const { pathname } = useLocation();
+
+  // 確認身份
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      // 如果 authToken 不存在，就要回到登入頁！
+      if (!authToken) {
+        setPayload(null);
+        setIsAuthenticated(false);
+        return;
+      }
+
+      // 確認 authToken 是不是有效的，這個會回傳 success 的布林值
+      const result = await checkPermission(authToken);
+      // 如果 authToken 無效，就要回到登入頁
+      if (result) {
+        setIsAuthenticated(true);
+        const tempPayload = jwt.decode(authToken);
+        setPayload(tempPayload);
+      } else {
+        setPayload(null);
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkTokenIsValid();
+  }, [pathname]);
+  // 如果當前頁面有改變，就觸發這個 useEffect
 
   return (
     <AuthContext.Provider
