41edcc05ece9084383571487d000f84562b1ebca
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-10 13:48:21 +0800

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,19 @@
+import { useAuthContext } from 'contexts/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
+  const { isAuthenticated } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    } else {
+      navigate('/login');
+    }
+  }, [isAuthenticated]);
+
   return <div>HomePage</div>;
 };
 
